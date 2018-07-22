6cac730f7568a03ddd07e5e4c020114e18245a2a
Oleg Topchiy <oleg.topchiy@schibsted.com>
2018-07-22 20:24:26 +0200

    bind to

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	server := &PlayerServer{NewInMemoryPlayerStore()}
+	port := "5000"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
 
-	if err := http.ListenAndServe(":5000", server); err != nil {
-		log.Fatalf("could not listen on port 5000 %v", err)
+	if err := http.ListenAndServe(":" + port, server); err != nil {
+		log.Fatalf("could not listen on port %s %v", port, err)
 	}
 }
